70a6a8d666e609df1f9eb226986ec26b6e979bca
suleimanapaspalova <157298073+suleimanapaspalova@users.noreply.github.com>
2025-03-19 05:42:32 -0700

    Writing new unit tests for notification service

--- notification-svc/src/main/java/app/web/dto/UpsertNotificationPreference.java
+++ notification-svc/src/main/java/app/web/dto/UpsertNotificationPreference.java
@@ -2,10 +2,12 @@ package app.web.dto;
 
 import app.model.NotificationType;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.UUID;
 @Data
+@Builder
 public class UpsertNotificationPreference {
 
     @NotNull
